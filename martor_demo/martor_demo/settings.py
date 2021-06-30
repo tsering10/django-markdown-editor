a0c9d8d5fb6309c0edda15ee041c442e4bb699ff
tashi tsering <tashi.cse@gmail.com>
2021-06-30 23:45:16 +0200

    changes added

--- martor_demo/martor_demo/settings.py
+++ martor_demo/martor_demo/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
-import tempfile
+# import tempfile
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -141,9 +141,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-# STATIC_ROOT = os.path.join(tempfile.gettempdir(), 'martor_static')
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-
-# MEDIA_ROOT = os.path.join(tempfile.gettempdir(), 'martor_media')
 DEFAULT_AUTO_FIELD='django.db.models.AutoField'
