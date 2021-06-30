71529f2b2646b0673823cd73ea6ecd0ac244c8d2
tashi tsering <tashi.cse@gmail.com>
2021-06-30 23:08:14 +0200

    add unit test and static files

--- martor_demo/martor_demo/settings.py
+++ martor_demo/martor_demo/settings.py
@@ -141,5 +141,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(tempfile.gettempdir(), 'martor_static')
-MEDIA_ROOT = os.path.join(tempfile.gettempdir(), 'martor_media')
+# STATIC_ROOT = os.path.join(tempfile.gettempdir(), 'martor_static')
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+# MEDIA_ROOT = os.path.join(tempfile.gettempdir(), 'martor_media')
+DEFAULT_AUTO_FIELD='django.db.models.AutoField'
